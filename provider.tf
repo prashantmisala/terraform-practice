8798aa25966a9de610d95459e6a1c7c28ab9a6f1
chitti <82209955+prashantmisala@users.noreply.github.com>
2025-09-03 13:39:24 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,6 +6,11 @@ terraform {
         version = "~> 3.0"
     }
   }
+  backend "s3" {
+      bucket = "testbuckt0000"
+      key    = "jenkins/terraform.tfstate"
+      region = "us-east-1"
+  }
 }
 provider "aws" {
     region = "us-east-1"
